11d53e55d81398f55bc2d25d7e961b597631caaa
Uristrix <mr.lux1@yandex.ru>
2023-04-09 14:08:27 +0300

    test 5

--- index.py
+++ index.py
@@ -6,21 +6,7 @@ from create_xlsx import *
 app = Flask(__name__)
 load_dotenv()
 
+
 @app.route('/')
-def home():
-    return 'Home Page Route'
-
-
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
+def main():
+    return render_template('index.html')
